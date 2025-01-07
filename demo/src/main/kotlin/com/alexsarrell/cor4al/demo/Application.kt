f5cf28436f7b940d39fdf57453ed7752bd4ff175
sunnyshechka <alexsarrell.workspace@gmail.com>
2025-01-07 21:09:37 +0300

    Add metadata accessors, loaderignore, importMappings and UUID mapping

--- demo/src/main/kotlin/com/alexsarrell/cor4al/demo/Application.kt
+++ demo/src/main/kotlin/com/alexsarrell/cor4al/demo/Application.kt
@@ -2,8 +2,9 @@ package com.alexsarrell.cor4al.demo
 
 import com.alexsarrell.demo.ChildEventFileImport
 import com.alexsarrell.demo.name
+import java.util.UUID
 
 fun main() {
-    val event = ChildEventFileImport()
+    val event = ChildEventFileImport(sessionId = UUID.randomUUID())
     event.name // extracts name from metadata
 }
--- generator/kotlin/src/main/kotlin/com/alexsarrell/cor4al/generator/kotlin/JavaImportMappings.kt
+++ generator/kotlin/src/main/kotlin/com/alexsarrell/cor4al/generator/kotlin/JavaImportMappings.kt
@@ -4,12 +4,14 @@ import com.alexsarrell.cor4al.core.model.TypeMappings
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
+import java.util.UUID
 
 object JavaImportMappings : TypeMappings {
     private val mappings = mutableMapOf<String, String>(
         "OffsetDateTime" to OffsetDateTime::class.javaObjectType.name,
         "LocalDateTime" to LocalDateTime::class.javaObjectType.name,
         "Instant" to Instant::class.javaObjectType.name,
+        "UUID" to UUID::class.javaObjectType.name,
     )
 
     override fun getMappings(): Map<String, String> {
